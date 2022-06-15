b2a1dd0e7520be44502ddcc86892b495bfb0466f
adrianongit <adriansantosrollim@gmail.com>
2022-06-15 18:06:38 -0300

    animation s menu settings

--- public/src/js/script.js
+++ public/src/js/script.js
@@ -1,6 +1,19 @@
-$(document).ready(function(){
+$(function(){
 
-	$('navbtn_box').click(function(){
-		
-	});
-});
+	/* Animação de scroll do menu */
+	let navContents = $('.nav_contents');
+	navContents.hide(0);
+
+	let menu = $('.menu_btn');
+	let close = $('.close_btn');
+	close.hide(0);
+
+	$('.navbtn_box').click(function(){
+
+		navContents.slideToggle('slow');
+		menu.toggle(0);
+		close.toggle(0)
+	})
+
+	/* Animação do botão do menu */
+})
